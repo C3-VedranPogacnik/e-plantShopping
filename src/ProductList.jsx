bbc12663f1b2be9f06ffda04dbf1d75255ce7b4b
C3-VedranPogacnik <vedran.pogacnik@c3.ai>
2025-04-03 20:04:50 -0700

    initial declarations

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
+    const cart = useSelector((state) => state.cart.items);
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const [disabled, setDisabled] = useState(false);
+    const dispatch = useDispatch();
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
